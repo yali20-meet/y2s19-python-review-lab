b8231793462f0477ab41d66f31875bb12f983a20
myname21-meet <myname21@meet.mit.edu>
2019-07-23 02:55:08 -0400

    did somestuff

--- part2.py
+++ part2.py
@@ -1,7 +1,35 @@
 # Part 2 of the Python Review lab.
 
+def is_prime(num):
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    return True
+
 def encode(x, y):
-	pass
+    while 1 < y < 250 and 500 < x < 1000:
+        while not is_prime(x):
+            x+=1
+
+        while not is_prime(y):
+            y+=1
+
+        product = x + y
+        return product
+
+    print("Ivalid input: Outside range.")
+    return None
 
 def decode(coded_message):
-	pass
+    num = coded_message
+    for i in range(1, 250):
+        for j in range(500, 1000):
+            if is_prime(i) and is_prime(j) and i+j == num:
+                print("x is %d ; y is %d" % (j , i))
+
+x , y = 789 , 45
+encode(x,y)
+decode(encode(x, y))
+
+
+        
--- part3.py
+++ part3.py
@@ -8,8 +8,8 @@ NUMBERS_TO_LETTERS = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h', 8:
 					  23: 'x', 24: 'y', 25: 'z'}
 
 def createShiftDictionary(s):
-	pass
-
+        pass
+                
 def encode(plaintext, s):
 	pass
 
